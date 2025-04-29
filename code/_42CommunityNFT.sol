4691c36f7f0fa7b01aea1609cfec0ed802bcda54
yassine-khadiri <yassinekhadiri77@gmail.com>
2025-04-29 17:24:37 +0100

    Adding The Mandatory Part

--- code/_42CommunityNFT.sol
+++ code/_42CommunityNFT.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract _42Community is ERC721URIStorage {
     
-    constructor(address _to, uint256 tokenId, string memory _tokenURI) ERC721("_42Community", "_42CY") {
-        _safeMint(_to, tokenId);
+    constructor(uint256 tokenId, string memory _tokenURI) ERC721("_42Community", "_42CY") {
+        _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, _tokenURI);
     }
 }
